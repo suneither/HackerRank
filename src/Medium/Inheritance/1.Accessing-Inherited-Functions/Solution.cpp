94b3a7020a73e3c504ded835e4ef68cfb7d2bdc6
suneither <dangis.bakutis@gmail.com>
2025-10-17 16:30:09 +0300

    feat: solved Accessing Inhgerited Functions problem

--- src/Medium/Inheritance/1.Accessing-Inherited-Functions/Solution.cpp
+++ src/Medium/Inheritance/1.Accessing-Inherited-Functions/Solution.cpp
@@ -5,23 +5,29 @@ using namespace std;
 class A
 {
 public:
-    A(){
+    A()
+    {
         callA = 0;
     }
+
 private:
     int callA;
-    void inc(){
+
+    void inc()
+    {
         callA++;
     }
 
 protected:
-    void func(int & a)
+    void func(int& a)
     {
         a = a * 2;
         inc();
     }
+
 public:
-    int getA(){
+    int getA()
+    {
         return callA;
     }
 };
@@ -29,22 +35,29 @@ public:
 class B
 {
 public:
-    B(){
+    B()
+    {
         callB = 0;
     }
+
 private:
     int callB;
-    void inc(){
+
+    void inc()
+    {
         callB++;
     }
+
 protected:
-    void func(int & a)
+    void func(int& a)
     {
         a = a * 3;
         inc();
     }
+
 public:
-    int getB(){
+    int getB()
+    {
         return callB;
     }
 };
@@ -52,30 +65,37 @@ public:
 class C
 {
 public:
-    C(){
+    C()
+    {
         callC = 0;
     }
+
 private:
     int callC;
-    void inc(){
+
+    void inc()
+    {
         callC++;
     }
+
 protected:
-    void func(int & a)
+    void func(int& a)
     {
         a = a * 5;
         inc();
     }
+
 public:
-    int getC(){
+    int getC()
+    {
         return callC;
     }
 };
 
-class D
+class D : A, B, C
 {
-
     int val;
+
 public:
     //Initially val is 1
     D()
@@ -83,25 +103,41 @@ public:
         val = 1;
     }
 
-
     //Implement this function
     void update_val(int new_val)
     {
-
-
+        while (new_val != 1)
+        {
+            if (new_val % 5 == 0)
+            {
+                C::func(val);
+                new_val /= 5;
+            }
+            else if (new_val % 3 == 0)
+            {
+                B::func(val);
+                new_val /= 3;
+            }
+            else if (new_val % 2 == 0)
+            {
+                A::func(val);
+                new_val /= 2;
+            }
+        }
     }
+
     //For Checking Purpose
     void check(int); //Do not delete this line.
 };
 
-
-
 void D::check(int new_val)
 {
     update_val(new_val);
-    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
+    cout << "Value = " << val << endl
+        << "A's func called " << getA() << " times" << endl
+        << "B's func called " << getB() << " times" << endl
+        << "C's func called " << getC() << " times" << endl;
 }
-
 
 int main()
 {
@@ -109,5 +145,4 @@ int main()
     int new_val;
     cin >> new_val;
     d.check(new_val);
-
 }
