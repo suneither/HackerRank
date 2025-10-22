ba5ee3895bcffb98dde56e5171ff75f421237e09
suneither <dangis.bakutis@gmail.com>
2025-10-22 12:16:27 +0300

    feat: solved Hotel Prices problem

--- src/Medium/Debugging/1.Hotel-Prices/Solution.cpp
+++ src/Medium/Debugging/1.Hotel-Prices/Solution.cpp
@@ -8,50 +8,65 @@ public:
     HotelRoom(int bedrooms, int bathrooms)
     : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
 
-    int get_price() {
-        return 50*bedrooms_ + 100*bathrooms_;
+    virtual int get_price()
+    {
+        return 50 * bedrooms_ + 100 * bathrooms_;
     }
 private:
     int bedrooms_;
     int bathrooms_;
 };
 
-class HotelApartment : public HotelRoom {
+class HotelApartment : public HotelRoom
+{
 public:
     HotelApartment(int bedrooms, int bathrooms)
-    : HotelRoom(bedrooms, bathrooms) {}
+        : HotelRoom(bedrooms, bathrooms)
+    {
+    }
 
-    int get_price() {
+    int get_price() override
+    {
         return HotelRoom::get_price() + 100;
     }
 };
 
-int main() {
+// Locked code by hacker rank
+int main()
+{
     int n;
     cin >> n;
     vector<HotelRoom*> rooms;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         string room_type;
-    int bedrooms;
+        int bedrooms;
         int bathrooms;
         cin >> room_type >> bedrooms >> bathrooms;
-        if (room_type == "standard") {
+        if (room_type == "standard")
+        {
             rooms.push_back(new HotelRoom(bedrooms, bathrooms));
-        } else {
+        }
+        else
+        {
             rooms.push_back(new HotelApartment(bedrooms, bathrooms));
         }
     }
 
     int total_profit = 0;
-    for (auto room : rooms) {
+    for (auto room : rooms)
+    {
         total_profit += room->get_price();
     }
-        cout << total_profit << endl;
+    cout << total_profit << endl;
 
-    for (auto room : rooms) {
+    for (auto room : rooms)
+    {
         delete room;
     }
     rooms.clear();
 
     return 0;
 }
+
+// Locked code by hacker rank
