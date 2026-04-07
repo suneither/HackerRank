5d1c51328b4a5e4eb56fcfd97ce1761d2063dc12
suneither <dangis.bakutis@gmail.com>
2026-04-07 13:39:50 +0300

    solved Messages Order Problem

--- src/Medium/Debugging/4.Messages-Order/Solution.cpp
+++ src/Medium/Debugging/4.Messages-Order/Solution.cpp
@@ -7,18 +7,30 @@ using namespace std;
 // locked by HackerRank
 
 class Message {
+private:
+    std::string _text;
+    int _id;
 public:
-    Message() {}
-    const string& get_text() {
+    Message(const string& text, const int id) : _text(text), _id(id) {}
 
+    const string& get_text() {
+        return _text;
+    }
+
+    bool operator<(const Message& message)const{
+        if(_id < message._id) return true;
+        return false;
     }
 };
 
 class MessageFactory {
+    int counter = 0;
 public:
-    MessageFactory() {}
-    Message create_message(const string& text) {
-
+    MessageFactory() = default;
+    Message create_message(const string& text)
+    {
+        counter++;
+        return {text, counter};
     }
 };
 
