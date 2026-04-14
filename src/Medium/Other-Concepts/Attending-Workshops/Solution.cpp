8ffd3b4ccbfffb58974a91e58715f87d11f8a38f
suneither <dangis.bakutis@gmail.com>
2026-04-14 18:18:01 +0300

    first failed try

--- src/Medium/Other-Concepts/Attending-Workshops/Solution.cpp
+++ src/Medium/Other-Concepts/Attending-Workshops/Solution.cpp
@@ -5,6 +5,58 @@ using namespace std;
 
 // hackerrank locked code
 
+struct Workshop
+{
+    int start_time{};
+    int duration{};
+    int end_time{};
+    Workshop()= default;
+    Workshop(const int start_time, const int duration):
+    start_time(start_time), duration(duration), end_time(start_time + duration){}
+
+    bool operator<(const Workshop& b) const
+    {
+        return start_time < b.start_time;
+    }
+};
+
+struct Available_Workshops
+{
+    int n;
+    vector<Workshop> workshops;
+    Available_Workshops(const int n): n(n) {}
+
+};
+
+Available_Workshops* initialize (int start_time[], int duration[], int n)
+{
+    auto* available_workshops = new Available_Workshops(n);
+    for (int i = 0; i < n; i++)
+    {
+        Workshop workshop(start_time[i],duration[i]);
+        available_workshops->workshops.push_back(workshop);
+    }
+    return available_workshops;
+}
+
+int CalculateMaxWorkshops(Available_Workshops* ptr)
+{
+    int result = 0;
+    sort(ptr->workshops.begin(), ptr->workshops.end());
+    for (int i = 0; i < ptr->n; i++)
+    {
+        for (int j = i + 1; j < ptr->n -1; j++)
+        {
+            if (ptr->workshops.at(i).end_time <= ptr->workshops.at(j).start_time)
+            {
+                result++;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
 //Define the structs Workshops and Available_Workshops.
 //Implement the functions initialize and CalculateMaxWorkshops
 
